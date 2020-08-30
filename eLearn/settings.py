14db352c5a8478585bea1aaec70534fcbdce5781
mirrorpie <raushankumar1047@pec.edu>
2020-08-31 01:13:05 +0530

    hosted_app

--- eLearn/settings.py
+++ eLearn/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0=%rs_oshwpo617@=^*@5*pr0u@*i+_@9173d$-zd$qq9-!y9b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['elearnfluent.herokuapp.com']
 
 
 # Application definition
